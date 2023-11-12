709449c844a5c12051a645fc0f208260d0437dc4
doxylee <dylee063@gmail.com>
2023-11-13 07:06:58 +0900

    wip: token refreshing

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -1,20 +1,39 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from '../decorator/skip-auth.decorator';
+import { AuthService } from '../auth.service';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
-  constructor(private reflector: Reflector) {
+export class JwtAuthGuard extends AuthGuard(['jwt', 'refresh']) implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    // private readonly authService: AuthService,
+  ) {
     super();
   }
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-    return isPublic ? true : super.canActivate(context);
+
+    if (isPublic ? true : super.canActivate(context)) {
+      // const req = context.switchToHttp().getRequest();
+      // const res = context.switchToHttp().getResponse();
+      // const userDTO = req.user;
+
+      // if (!req.cookies.jwt) {
+      //   // Access token is expired
+      //   // TODO: Actually check instead of just checking existance
+      //   const access = await this.authService.getAccessTokenAndOptions(userDTO);
+      //   const refresh = await this.authService.getRefreshTokenAndOptions(userDTO);
+
+      //   // TODO: Update refresh token in db
+      //   res.cookie('jwt', access.token, access.options);
+      // }
+    }
+    return false;
   }
 }
